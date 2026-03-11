78362098bd2d33523e156fa7466408e8e3b0bd6a
Cesar Luis <cesarluissalazar30106@hotmail.com>
2026-03-10 22:14:43 -0600

    Subida del proyecto a repositorio

---
+++ src/test/java/com/utrng/sigecc/SigeccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utrng.sigecc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigeccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
